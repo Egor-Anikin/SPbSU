512ff7948f280d96ea670b69129c3f52effd9dd0
egor-anikin <egor-anikin@inbox.ru>
2018-11-04 22:36:45 +0300

    Fix tests

--- academic/test3/src/test/java/spbsu/test3/RBTreeTest.java
+++ academic/test3/src/test/java/spbsu/test3/RBTreeTest.java
@@ -11,22 +11,23 @@ public class RBTreeTest {
         RBTree<Integer, String> tree = new RBTree<>();
     }
 
-    /** Function test add and remove.*/
+    /** Function test add and remove. */
     @Test
     public void addRemovTest() {
         RBTree<Integer, String> tree = new RBTree<>();
 
-        tree.add(6, "asdfghj");
-        tree.add(1, "asdfghj");
-        tree.add(7, "asdfghj");
-        tree.add(5, "asdfghj");
-        tree.add(9, "asdfghj");
-        tree.add(3, "asdfghj");
-        tree.add(2, "asdfghj");
-        tree.add(8, "asdfghj");
-        tree.add(4, "asdfghj");
+        tree.add(6, "test");
+        tree.add(1, "test");
+        tree.add(7, "test");
+        tree.add(5, "test");
+        tree.add(9, "test");
+        tree.add(3, "test");
+        tree.add(2, "test");
+        tree.add(8, "test");
+        tree.add(4, "test");
+
         for (int i=1; i < 10; i++) {
-            assertEquals("find isn't work!", "asdfghj",tree.show(i));
+            assertEquals("Add isn't work!", "test", tree.show(i));
         }
 
         tree.remove(6);
@@ -40,16 +41,15 @@ public class RBTreeTest {
         tree.remove(5);
 
         for (int i=1; i < 10; i++) {
-            assertEquals("find isn't work!", null,tree.show(i));
+            assertEquals("Remove isn't work!", null, tree.show(i));
         }
-
     }
 
     /** Empty tree test. */
     @Test
     public void nullTest() {
         RBTree<Integer, String> tree = new RBTree<>();
-        assertEquals("find nonexisten element", null,tree.show(6));
+        assertEquals("Find nonexisten element", null, tree.show(6));
         tree.remove(6);
     }
 
@@ -57,12 +57,12 @@ public class RBTreeTest {
     @Test
     public void strigTest() {
         RBTree<String, String> tree = new RBTree<>();
-        tree.add("ab", "asdfghj");
-        tree.add("a", "asdfghj");
-        tree.add("bca", "asdfghj");
-        assertEquals("not found element", "asdfghj",tree.show("ab"));
-        assertEquals("not found element", "asdfghj",tree.show("a"));
-        assertEquals("not found element", "asdfghj",tree.show("bca"));
+        tree.add("ab", "test");
+        tree.add("a", "test");
+        tree.add("bca", "test");
+        assertEquals("Not found element", "test", tree.show("ab"));
+        assertEquals("Not found element", "test", tree.show("a"));
+        assertEquals("Not found element", "test", tree.show("bca"));
         tree.remove("ab");
         tree.remove("a");
         tree.remove("bca");
@@ -73,11 +73,11 @@ public class RBTreeTest {
     public void rotateLeftTest() {
         RBTree<Integer, String> tree = new RBTree<>();
 
-        tree.remove(6);
-        tree.remove(3);
-        tree.remove(7);
-        tree.remove(4);
-        tree.remove(5);
+        tree.add(6, "test");
+        tree.add(3, "test");
+        tree.add(7, "test");
+        tree.add(4, "test");
+        tree.add(5, "test");
     }
 
     /** Rotate right test. */
@@ -85,11 +85,11 @@ public class RBTreeTest {
     public void rotateRightTest() {
         RBTree<Integer, String> tree = new RBTree<>();
 
-        tree.remove(6);
-        tree.remove(3);
-        tree.remove(7);
-        tree.remove(2);
-        tree.remove(1);
+        tree.add(6, "test");
+        tree.add(3, "test");
+        tree.add(7, "test");
+        tree.add(2, "test");
+        tree.add(1, "test");
     }
 
 }
